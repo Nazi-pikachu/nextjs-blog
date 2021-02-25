4d4410f5ff16608a344bde13f247e448ed5b5889
Rishabh Jain <jain.rishabhche@gmail.com>
2021-02-25 22:07:28 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -28,10 +28,6 @@ export default function Home({ allPostsData }) {
           there I have be on a amazing roller coaster ride of learning all the
           stuff on my own!!!.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
